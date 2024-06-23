63f0c0138c333a83f8a4a279dbb19e309a816cd4
Sosu Alfred <sosualfred@gmail.com>
2024-06-23 17:07:17 +0000

    set up catalogue api

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -7,20 +7,36 @@ use Illuminate\Http\Request;
 
 class CatalogueController extends Controller
 {
-    public function index () {
+    public function index()
+    {
         return Catalogue::all();
     }
 
-    public function create () {
-        return view('catalogue.create');
+    public function show(int $id)
+    {
+        return Catalogue::find($id);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         Catalogue::create([
             'name' => $request->name,
         ]);
-        return redirect(route('catalogue.all'));
 
-        // return response('Catalogue added successfully', 200);
+        return response('Catalogue added successfully', 201);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $catalogue = Catalogue::find($id);
+        $catalogue->update([
+            'name' => $request->name,
+        ]);
+    }
+
+    public function destroy(int $id)
+    {
+        $catalogue = Catalogue::find($id);
+        $catalogue->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\CatalogueController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/catalogues', [CatalogueController::class, 'index']);
+Route::get('/catalogues/{id}', [CatalogueController::class, 'show']);
+Route::post('/catalogues', [CatalogueController::class, 'store']);
+Route::patch('/catalogues/{id}', [CatalogueController::class, 'update']);
+Route::delete('/catalogues/{id}', [CatalogueController::class, 'destroy']);
